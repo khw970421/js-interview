d2a3ff86fddf3ed2cea6eed2d15607b2db9ceecf
khw970421 <khw970421@kakao.com>
2020-07-21 23:05:15 +0900

    7/18 this,명시적 this 바인딩, 리터럴, 화살표함수(정적 this) vs 일반함수(동적 this)

--- const/project3.js
+++ const/project3.js
@@ -16,6 +16,7 @@ console.log(k());
 //기본적으로 this는 내부함수, 콜백함수 등 여러 개에  전역객체로 바인딩된다.
 
 //어떤함수의 메소드일 경우에는 this는 자신을 호출한 객체에 바인딩, new를 이용한 생성자함수의 this 또한 생성자 함수에 의해 생성된 인스턴스(전역객체 x)
+//즉 생성자 함수와 객체의 메소드를 제외한 모든 함수(내부 함수, 콜백 함수 포함) 내부의 this는 전역 객체를 가리킨다
 
 let value = 1;
 
@@ -96,3 +97,44 @@ add.call(o, 5, 7); // 16
 // 첫 번째 인자는 'this'로 사용할 객체이고,
 // 두 번째 인자는 함수 호출에서 인수로 사용될 멤버들이 위치한 배열이다.
 add.apply(o, [10, 20]); // 34
+
+/* 화살표 함수(this에 바인딩할 객체가 정적)  vs 일반 함수(this에 바인딩할 객체가 동적) => this의 차이
+함수를 선언할 때 this에 바인딩할 객체가 정적으로 결정되는 것이 아니고,
+함수를 호출할 때 함수가 어떻게 호출되었는지에 따라 this에 바인딩할 객체가 동적으로 결정된다.
+화살표 함수는 this와 arguments를 바인딩하지 않는다
+화살표 함수는 this와 arguments를 바인딩하지 않는다. 그 대신, 일반적인 this와 arguments와 동일한 범위를 가지고 있다.
+ */
+
+function createObject() {
+    console.log('Inside `createObject`:', this.foo, this);
+    return {
+        foo: 42,
+        bar: function() {
+            console.log('Inside `bar`:', this.foo, this);
+        },
+    };
+}
+createObject.call({foo: 21}).bar();
+
+/*
+Inside `createObject`: 21, {foo: 21}
+Inside `bar`: 42, bar: f
+*/
+
+function createObject() {
+    console.log('Inside `createObject`:', this.foo, this);
+    return {
+        foo: 42,
+        bar: () => console.log('Inside `bar`:', this.foo, this),
+    };
+}
+createObject.call({foo: 21}).bar();
+
+/*
+Inside `createObject`: 21, {foo: 21}
+Inside `bar`: 21, {foo: 21}
+ */
+//즉 화살표 함수가 현재 환경의 this를 따르게 하고 싶을 때 유용하다
+
+
+//출처 : https://www.yceffort.kr/2020/05/difference-between-function-and-arrow/
--- const/project4.js
+++ const/project4.js
@@ -1,15 +1,11 @@
-function add(c, d) {
-    return this.a + this.b;
+function Prefixer(prefix) {
+    this.prefix = prefix;
 }
 
-var o = {a: 1, b: 3};
+Prefixer.prototype.prefixArray = function (arr) {
+    // this는 상위 스코프인 prefixArray 메소드 내의 this를 가리킨다.
+    return arr.map(x => `${this.prefix}  ${x}`);
+};
 
-// 첫 번째 인자는 'this'로 사용할 객체이고,
-// 이어지는 인자들은 함수 호출에서 인수로 전달된다.
-add.call(o, 5, 7); // 16
-
-// 첫 번째 인자는 'this'로 사용할 객체이고,
-// 두 번째 인자는 함수 호출에서 인수로 사용될 멤버들이 위치한 배열이다.
-add.apply(o, [10, 20]); // 34
-
-console.log(add.call(o))
+const pre = new Prefixer('Hi');
+console.log(pre.prefixArray(['Lee', 'Kim']));
